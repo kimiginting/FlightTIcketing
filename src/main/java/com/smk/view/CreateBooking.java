8644d528f271e4d1331812c1f3be0708b65428b4
kimiginting <kimiginting@hotmail.com>
2023-11-22 13:31:53 +0700

    Vaadin Integration With Database

--- src/main/java/com/smk/view/CreateBooking.java
+++ src/main/java/com/smk/view/CreateBooking.java
@@ -1,6 +1,8 @@
 package com.smk.view;
 
 import com.smk.MainView;
+import com.smk.dao.LocationDao;
+import com.smk.model.Location;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -12,15 +14,23 @@ import com.vaadin.flow.router.Route;
 @PageTitle("Create Booking")
 @Route(value = "create-booking", layout = MainView.class)
 public class CreateBooking extends VerticalLayout {
+    private LocationDao locationDao;
     public CreateBooking() {
+        locationDao = new LocationDao();
         createForm();
     }
 
     private void createForm(){
         setAlignItems(Alignment.STRETCH);
 
-        ComboBox fromComboBox = new ComboBox ("Dari");
-        ComboBox toComboBox = new ComboBox ("Ke");
+        ComboBox<Location> fromComboBox = new ComboBox("Dari");
+        fromComboBox.setItems(locationDao.getAll());
+        fromComboBox.setItemLabelGenerator(Location::getName);
+
+        ComboBox<Location> toComboBox = new ComboBox("Ke");
+        toComboBox.setItems((locationDao.getAll()));
+        toComboBox.setItemLabelGenerator(Location::getName);
+
         DatePicker depatureDatePicker = new DatePicker("Tanggal Keberangkatan");
         DatePicker arrivalDatePicker = new DatePicker("Tanggal Kepulangan");
         Button searchButton = new Button("Search");
